9a10224410d5a6677cfae26284282c7881b076aa
Salazar <Salazar@Salazar.mshome.net>
2020-05-18 17:15:54 -0500

    correccion

--- MetodoLagrange/src/metodolagrange/MetodoLagrange.java
+++ MetodoLagrange/src/metodolagrange/MetodoLagrange.java
@@ -3,8 +3,8 @@ package metodolagrange;
 public class MetodoLagrange {
 
     public static void main(String[] args) {
-        //frmVentana v = new frmVentana();
-        //v.setVisible(true);
+        frmVentana v = new frmVentana();
+        v.setVisible(true);
     }
 
 }
